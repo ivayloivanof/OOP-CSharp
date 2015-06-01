0e1c7acfeb7b66d3cfa76ca1e2f7e4f7311bbf81
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-01 20:40:12 +0300

    Add DefiningClass project and decide

---
+++ 01.DefiningClasses/Exercise/DefiningClass/DefiningClass/Dog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DefiningClass
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+
+        public Dog() : this(null, null)
+        {
+            
+        }
+
+        //public Dog() : 
+
+        public Dog(string name, string breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("{0} ({1}) said: Bauuuuuu!", this.Name ?? "[unnamed dog]", this.Breed ?? "[uncnow breed]");
+        }
+    }
+}
---
+++ 01.DefiningClasses/Exercise/DefiningClass/DefiningClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DefiningClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dog unnamed = new Dog();
+            Dog sharo = new Dog("Sharo", "Melez");
+
+            unnamed.Breed = "German Shepherd";
+
+            unnamed.Bark();
+            sharo.Bark();
+
+        }
+    }
+}
