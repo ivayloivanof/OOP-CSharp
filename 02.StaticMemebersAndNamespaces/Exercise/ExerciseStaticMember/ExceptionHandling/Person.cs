10b1f85f9b60b9f12dcd0ae2b66c8af548fd84c3
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-03 21:37:20 +0300

    decide exercise 2

---
+++ 02.StaticMemebersAndNamespaces/Exercise/ExerciseStaticMember/ExceptionHandling/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Person
+    {
+
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "Tha value is null or empty!");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("value", "Tha value is null or empty!");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0 && value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("value", "This number is negative or too big!");
+                }
+            }
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+}
