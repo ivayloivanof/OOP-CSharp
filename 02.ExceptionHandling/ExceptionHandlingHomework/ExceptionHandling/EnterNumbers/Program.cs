86d610cf675d81fbfed2a900438aec6cb6ed8eb3
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-04 23:42:07 +0300

    Exception Handling decided all problems and add archive

---
+++ 02.ExceptionHandling/ExceptionHandlingHomework/ExceptionHandling/EnterNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop = 1;
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+            while (loop <= 10)
+            {
+                int num = int.Parse(Console.ReadLine());
+                try
+                {
+                    if (ReadNumber(numbers[0], numbers[1]))
+                    {
+                        if (num <= numbers[0] || num >= numbers[1])
+                        {
+                            throw new ArgumentOutOfRangeException("Number is out of range");
+                        }
+                        Console.WriteLine(num);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Invalid number");
+                    ;
+                }
+                finally
+                {
+                    loop++;
+                }
+            }
+        }
+
+        private static bool ReadNumber(int start, int end)
+        {
+            if (start < 1)
+                throw new ArgumentOutOfRangeException("Start number is out of range");
+            if (end > 100)
+                throw new ArgumentOutOfRangeException("End number is out of range");
+            return true;
+        }
+    }
+}
---
+++ 02.ExceptionHandling/ExceptionHandlingHomework/ExceptionHandling/SquareRoot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+            SqareRoot(number);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Invalid number");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+
+    private static void SqareRoot(int number)
+    {
+        if (number < 0)
+            throw new ArgumentOutOfRangeException("Number is negative!");
+        Console.WriteLine(Math.Sqrt(number));
+    }
+}
