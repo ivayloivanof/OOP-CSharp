87bc60ad4526585eea8605f31de6b0acfe0e70ff
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-06 15:43:46 +0300

    Change Point3D and decide

--- 02.StaticMemebersAndNamespaces/StaticMembersAndNamespace/StaticMemberAndNamespace/Point3D/Point3D.cs
+++ 02.StaticMemebersAndNamespaces/StaticMembersAndNamespace/StaticMemberAndNamespace/Point3D/Point3D.cs
@@ -1,11 +1,68 @@
-﻿namespace Point3D
+﻿using System;
+namespace Point3D
 {
-    class Point3D
+    public class Point3D
     {
+        private static readonly int[] startingPoints = {0,0,0};
+        public static int[] StartingPoints
+        {
+            get { return startingPoints; }
+        }
+
         private double x;
         private double y;
         private double z;
 
+        public double X
+        {
+            get { return this.x; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                if (value == null)
+                    throw new ArgumentNullException();
 
+                this.x = value;
+            }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                if (value == null)
+                    throw new ArgumentNullException();
+
+                this.y = value;
+            }
+        }
+        public double Z
+        {
+            get { return this.z; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException();
+                if (value == null)
+                    throw new ArgumentNullException();
+
+                this.z = value;
+            }
+        }
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public override string ToString()
+        {
+            return (x).ToString()+y+z;
+        }
     }
 }
--- 02.StaticMemebersAndNamespaces/StaticMembersAndNamespace/StaticMemberAndNamespace/Point3D/Program.cs
+++ 02.StaticMemebersAndNamespaces/StaticMembersAndNamespace/StaticMemberAndNamespace/Point3D/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
 namespace Point3D
 {
     class Program
     {
         static void Main()
         {
-            
+            Point3D points = new Point3D(3, 6, 5);
+            Console.WriteLine(points);
         }
     }
 }
