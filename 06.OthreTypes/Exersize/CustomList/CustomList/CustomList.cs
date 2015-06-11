b4bcf1231f4eb4d84d6866c014ffa4617fc1e1d0
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-11 21:54:00 +0300

    Add exercise in Other Types

---
+++ 06.OthreTypes/Exersize/CustomList/CustomList/CustomList.cs
@@ -0,0 +1,50 @@
+﻿namespace CustomList
+{
+    class CustomList <T>
+    {
+        const int defaultMemory = 3;
+        private T[] elements;
+        private int buffer;
+        private int elementIndex;
+
+        public CustomList(int memory = defaultMemory)
+        {
+            this.elements = new T[defaultMemory];
+            this.buffer = memory;
+            this.elementIndex = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (this.elementIndex >= elements.Length)
+            {
+                buffer = buffer*2;
+                T[] e = new T[buffer];
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    e[i] = this.elements[i];
+                }
+                this.elements = e;
+            }
+            this.elements[this.elementIndex] = element;
+            this.elementIndex++;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.elements[i] = default (T);
+            }
+            buffer = defaultMemory;
+            this.elementIndex = 0;
+            this.elements = new T[defaultMemory];
+        }
+
+        public int Count()
+        {
+            return this.elementIndex;
+        }
+
+    }
+}
---
+++ 06.OthreTypes/Exersize/CustomList/CustomList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomList<int> numbers = new CustomList<int>();
+            numbers.Add(6);
+            numbers.Add(23);
+            Console.WriteLine(numbers.Count());
+            numbers.Add(325);
+            numbers.Add(32532);
+            numbers.Add(235325);
+            Console.WriteLine(numbers.Count());
+            numbers.Add(32535);
+            numbers.Add(325325);
+
+            numbers.Clear();
+
+            //List<int> num = new List<int>();
+            //num.
+        }
+    }
+}
