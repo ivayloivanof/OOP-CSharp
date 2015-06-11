7bffbf1ee30c09d602bd69c4080493fccd23aac2
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-11 23:35:56 +0300

    Create Shapes project

--- 04.EncapsulationAndPolymorphism/EncapsulationAndPolimophism/Shapes/Program.cs
+++ 04.EncapsulationAndPolymorphism/EncapsulationAndPolimophism/Shapes/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Shapes
+﻿using System;
+using Shapes.Class;
+
+namespace Shapes
 {
+    using Interfaces;
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            IShape circle = new Circle(4);
+            Console.WriteLine(circle.CalculateArea());
+            circle.CalculatePerimeter();
+            IShape triangle = new Triangle(5, 8.4, 6.4, 8.5);
+            triangle.CalculateArea();
+            triangle.CalculatePerimeter();
+            IShape rectangle = new Rectangle(8, 5);
+            rectangle.CalculateArea();
+            rectangle.CalculatePerimeter();
+            Console.WriteLine();
         }
     }
 }
