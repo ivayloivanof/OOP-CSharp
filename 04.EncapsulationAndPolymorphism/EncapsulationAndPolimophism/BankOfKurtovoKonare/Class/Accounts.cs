84739d27d094766550a4666c16631cafcddde2c3
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-12 23:31:11 +0300

    update BankOfKurtovoKonare

--- 04.EncapsulationAndPolymorphism/EncapsulationAndPolimophism/BankOfKurtovoKonare/Class/Accounts.cs
+++ 04.EncapsulationAndPolymorphism/EncapsulationAndPolimophism/BankOfKurtovoKonare/Class/Accounts.cs
@@ -1,9 +1,31 @@
-﻿namespace BankOfKurtovoKonare.Class
+﻿using System;
+
+namespace BankOfKurtovoKonare.Class
 {
-    class Accounts
+    abstract class Accounts
     {
         private string customer;
-        private decimal balance;
-        private int interestRate;
+        protected decimal balance;
+        protected int interestRate;
+
+        public Accounts(string customer)
+        {
+            this.Customer = customer;
+        }
+
+        public string Customer
+        {
+            get { return this.customer; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("This customer not valid!");
+                this.customer = value.Trim();
+            }
+        }
+        public abstract decimal Balance { get; set; }
+        public abstract int  InterestRate { get; set; }
+
+
     }
 }
