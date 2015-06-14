cc31b842b50a2b3c3136dcd03c89bf1fadd8ad68
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-14 15:20:00 +0300

    add fields and property in FractionCalculator

---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/FractionCalculator/Class/Fraction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FractionCalculator.Class
+{
+    public struct Fraction
+    {
+        private long numerator;
+        private long denominator;
+
+        public Fraction(long numerator, long denominator) : this()
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public long Denominator
+        {
+            get { return this.denominator; }
+            set
+            {
+                if (value <= 0)
+                    throw new DivideByZeroException("Denominator cannot be zero or negative");
+                this.denominator = value;
+            }
+        }
+
+        public long Numerator
+        {
+            get { return this.numerator; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("numerator", "Numerator cannot be zero or negative!");
+                this.numerator = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format();
+        }
+    }
+}
---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/FractionCalculator/TestProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FractionCalculator
+{
+    public class TestProgram
+    {
+        public static void Main()
+        {
+            try
+            {
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
