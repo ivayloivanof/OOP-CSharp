950395c29a279aeca817a1f391e6bcdaaa6f6120
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-15 00:03:51 +0300

    add GenericListVersion

---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/GenericListVersion/Class/GenericList.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericList.Class
+{
+    class GenericList<T> : IComparable<T>
+    {
+        private const int defaultmemory = 5;
+        private T[] elements;
+        private int buffer;
+        private int elementIndex;
+
+        [Obsolete("Version: 2.11")]
+        public GenericList(int memory = defaultmemory)
+        {
+            this.elements = new T[defaultmemory];
+            this.Buffer = memory;
+        }
+
+        public int Buffer
+        {
+            get { return this.buffer; }
+            set
+            {
+                if (value < defaultmemory)
+                    this.buffer = defaultmemory;
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("buffer", "Buffer cannot be negative!");
+                this.buffer = value;
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (this.elementIndex >= elements.Length)
+            {
+                buffer = buffer * 2;
+                T[] e = new T[buffer];
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    e[i] = this.elements[i];
+                }
+                this.elements = e;
+            }
+            this.elements[this.elementIndex] = element;
+            this.elementIndex++;
+        }
+
+        public void Remove(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index", "Index cannot be negative!");
+            }
+            this.elements[index] = default (T);
+            for (int i = index; i < elements.Length - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+            this.elements[elements.Length-1] = default (T);
+        }
+
+        public int Finding(T value)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (this.elements[i].Equals(value))
+                {
+                    return i;   //return position
+                }
+            }
+            return -1;  //not founded
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.elements[i] = default(T);
+            }
+            this.buffer = defaultmemory;
+            this.elementIndex = 0;
+            this.elements = new T[defaultmemory];
+        }
+
+        public int Count()
+        {
+            return this.elementIndex;
+        }
+
+        public T Max()
+        {
+            if (this.elementIndex == 0)
+            {
+                throw new InvalidOperationException("GenericList is empty");
+            }
+
+            T max = this.elements[0];
+            for (int i = 1; i < this.elementIndex; i++)
+            {
+                //T current = this.elements[i];
+                //if (current.CompareTo(max) == 1)
+                //{
+                //    max = current;
+                //}
+            }
+
+            return max;
+        }
+
+        public T Min()
+        {
+            T tmp = elements[0];
+            for (int i = 0; i < elements.Length-1; i++)
+            {
+                
+            }
+            return tmp;
+        }
+
+        public int CompareTo(T other)
+        {
+            if (other == null) return 1;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(string.Join(", ", (from item in elements
+                                                    where !item.Equals(default (T))
+                                                    select item)));
+        }
+    }
+}
---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/GenericListVersion/TestProgram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GenericList
+{
+    using Class;
+    class TestProgram
+    {
+        static void Main()
+        {
+            try
+            {
+                GenericList<int> numbers = new GenericList<int>();
+                numbers.Add(6);
+                numbers.Add(23);
+                Console.WriteLine(numbers.Count());
+                numbers.Add(325);
+                numbers.Add(32532);
+                numbers.Add(235325);
+                Console.WriteLine(numbers.Count());
+                numbers.Add(32535);
+                numbers.Add(325565);
+
+                numbers.Remove(2);
+                int num = numbers.Finding(325325);
+                int max = numbers.Max();
+                int min = numbers.Min();
+
+                Console.WriteLine(numbers);
+                numbers.Clear();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
