5c0fe63a2b3c89b5d374e4f8e2b64c88e079d290
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-14 23:04:15 +0300

    add few methods (Add, Remove, Clear, Count)

---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/GenericList/Class/GenericList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace GenericList.Class
+{
+    class GenericList<T>
+    {
+        private const int defaultmemory = 5;
+        private T[] elements;
+        private int buffer;
+        private int elementIndex;
+
+        public GenericList(int memory = defaultmemory)
+        {
+            this.elements = new T[defaultmemory];
+            this.Buffer = memory;
+        }
+
+        public int Buffer
+        {
+            get { return this.buffer; }
+            set
+            {
+                if (value < defaultmemory)
+                    this.buffer = defaultmemory;
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("buffer", "Buffer cannot be negative!");
+                this.buffer = value;
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (this.elementIndex >= elements.Length)
+            {
+                buffer = buffer * 2;
+                T[] e = new T[buffer];
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    e[i] = this.elements[i];
+                }
+                this.elements = e;
+            }
+            this.elements[this.elementIndex] = element;
+            this.elementIndex++;
+        }
+
+        public void Remove(int index)
+        {
+            this.elements[index] = default (T);
+            for (int i = index; i < elements.Length - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+            this.elements[elements.Length-1] = default (T);
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                this.elements[i] = default(T);
+            }
+            this.buffer = defaultmemory;
+            this.elementIndex = 0;
+            this.elements = new T[defaultmemory];
+        }
+
+        public int Count()
+        {
+            return this.elementIndex;
+        }
+
+
+
+    }
+}
---
+++ 06.OthreTypes/06.OtherTypes.Homework/OtherTypes/GenericList/TestProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GenericList
+{
+    using Class;
+    class TestProgram
+    {
+        static void Main()
+        {
+            try
+            {
+                GenericList<int> numbers = new GenericList<int>();
+                numbers.Add(6);
+                numbers.Add(23);
+                Console.WriteLine(numbers.Count());
+                numbers.Add(325);
+                numbers.Add(32532);
+                numbers.Add(235325);
+                Console.WriteLine(numbers.Count());
+                numbers.Add(32535);
+                numbers.Add(325325);
+
+                numbers.Remove(2);
+
+                numbers.Clear();
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
