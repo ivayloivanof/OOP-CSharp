6ba9be44d54f927cccf8611105a466b7e7fa0b87
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-15 20:39:10 +0300

    add exercise predicate method - stupid

---
+++ 07.DelegatesAndEvents/Exercise/Exercise.DelegatesAndEvents/Exercise.DelegatesAndEvents/Prediacates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise.DelegatesAndEvents
+{
+    public static class Prediacates
+    {
+        public static T FirstOrDefault<T>(
+                        this IEnumerable<T> collection, 
+                        Predicate<T> condition)
+        {
+            foreach (var item in collection)
+            {
+                if (condition(item))
+                {
+                    return item;
+                }
+            }
+            return default(T);
+        }
+    }
+}
---
+++ 07.DelegatesAndEvents/Exercise/Exercise.DelegatesAndEvents/Exercise.DelegatesAndEvents/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise.DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> collection = new List<int> { 1, 2, 3, 4, 6, 11, 3};
+
+            Console.WriteLine(collection.FirstOrDefault(x => x > 7));
+        }
+    }
+}
