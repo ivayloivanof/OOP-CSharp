d348b928de7eff8bc16119a205bd8093d5a6b63c
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-20 22:28:45 +0300

    Decide InterestCalculator

---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/CustomLinqExtensionMethods/Students.cs
@@ -0,0 +1,18 @@
+﻿namespace CustomLinqExtensionMethods
+{
+    class Students
+    {
+        private string name;
+        private int grade;
+
+        public Students(string name, int grade)
+        {
+            this.Name = name;
+            this.Grade = grade;
+        }
+
+        public string Name { get; set; }
+
+        public int Grade { get; set; }
+    }
+}
---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/InterestCalculator/TestProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InterestCalculator
+{
+    class TestProgram
+    {
+        private const int InterestCompounded = 12;
+
+        static void Main()
+        {
+            Func<decimal, decimal, int, decimal> simple = GetSimpleInterest;
+            Func<decimal, decimal, int, decimal> compound = GetCompoundInterest;
+
+            InterestCalculator icalc1 = new InterestCalculator(500m, 5.6m, 10, compound);
+            InterestCalculator icalc2 = new InterestCalculator(2500m, 7.2m, 15, simple);
+
+            Console.WriteLine(icalc1);
+            Console.WriteLine(icalc2);
+        }
+
+        private static decimal GetCompoundInterest(decimal sum, decimal interest, int years)
+        {
+            return sum * (decimal)Math.Pow((double)(1 + (interest / 100 / InterestCompounded)), years * InterestCompounded);
+        }
+
+        private static decimal GetSimpleInterest(decimal sum, decimal interest, int years)
+        {
+            return sum *(1 + (interest / 100 * years));
+        }
+    }
+}
