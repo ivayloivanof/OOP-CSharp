7510420933ec4171397f6ebee6604bb70dfad762
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-20 22:43:06 +0300

    Add and decide StudentChanged project;

---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/StudentChanget/Student.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace StudentChanged
+{
+    public delegate void OnPropertyChangeEventHandler(Student student, ChangedEventArgs args);
+
+    public class Student
+    {
+        public event OnPropertyChangeEventHandler PropertyChanged;
+
+        private string name;
+
+        private int age;
+
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this,
+                        new ChangedEventArgs("Name", this.name, value));
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this,
+                        new ChangedEventArgs("Age", this.age, value));
+                }
+
+                this.age = value;
+            }
+        }
+    }
+}
