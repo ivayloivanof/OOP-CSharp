212d98c722348c794a65a48851af348177c315a2
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-20 22:09:05 +0300

    Create and decide first problem CustomLinqExtensionMethods;

---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/CustomLinqExtensionMethods/Predicate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CustomLinqExtensionMethods
+{
+    public static class Predicate
+    {
+        public static IEnumerable<T> WhareNot<T>(this IEnumerable<T> collection, Func<T, bool> predicate )
+        {
+            List<T> result = new List<T>();
+            foreach (T item in collection)
+            {
+                if (!predicate(item))
+                {
+                    result.Add(item);
+                }
+            }
+            if (result.Count == 0)
+                return (IEnumerable<T>) default(T);
+            return result;
+        }
+    }
+}
---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/CustomLinqExtensionMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomLinqExtensionMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> nums = new List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9 , 10};
+            var filteredCollection = nums.WhareNot(x => x%2 == 0);
+            Console.WriteLine(string.Join(", ", filteredCollection));
+        }
+    }
+}
