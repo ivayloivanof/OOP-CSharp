d4393890feac74da0ec24b2d7554888600bb7d3f
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-20 22:42:31 +0300

    Update AsyncTimer;

---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/AsyncTimer/TestProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AsyncTimer
+{
+    class TestProgram
+    {
+        static void Main()
+        {
+            Timer printLetter = new Timer(PrintLetterOnConsole, 3, 580);
+            printLetter.Execute();
+
+            Timer printNumber = new Timer(PrintNumberOnConsole, 3, 1250);
+            printNumber.Execute();
+        }
+
+        private static void PrintLetterOnConsole(int i)
+        {
+            char letter = (char)('a' + i);
+            Console.WriteLine(letter);
+        }
+
+        private static void PrintNumberOnConsole(int i)
+        {
+            Console.WriteLine(i + 2);
+        }
+    }
+}
---
+++ 07.DelegatesAndEvents/DelegatesAndEvents.Homework/DelegatesAndEvents/AsyncTimer/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncTimer
+{
+    class Timer
+    {
+        private int interval;
+        private int ticks;
+
+        public Timer(Action<int> action, int ticks, int interval)
+        {
+            this.Action = action;
+            this.Ticks = ticks;
+            this.Interval = interval;
+        }
+
+        public Action<int> Action { get; private set; }
+
+        public int Ticks
+        {
+            get { return this.ticks; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("ticks", "Ticks cannot be negative!");
+                this.ticks = value;
+            }
+        }
+
+        public int Interval
+        {
+            get { return this.interval; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("interval", "Interval cannot be negative!");
+                this.interval = value;
+            }
+        }
+
+        public void Execute()
+        {
+            Thread paralelThread = new Thread(this.Run);
+            paralelThread.Start();
+        }
+
+        public void Run()
+        {
+            for (int i = 0; i < this.Ticks; i++)
+            {
+                Thread.Sleep(this.Interval);
+
+                if (this.Action != null)
+                    this.Action(i);
+            }
+        }
+    }
+}
