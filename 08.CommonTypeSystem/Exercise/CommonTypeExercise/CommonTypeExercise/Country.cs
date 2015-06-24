1a972804fd06486edddb78853d58037cdcb280b6
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-24 22:07:57 +0300

    Add 09.Lab Folder and Files

---
+++ 08.CommonTypeSystem/Exercise/CommonTypeExercise/CommonTypeExercise/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CommonTypeExercise
+{
+    public class Country : ICloneable, IComparable
+    {
+        public Country(string name, int population, int area)
+        {
+            
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 08.CommonTypeSystem/Exercise/CommonTypeExercise/CommonTypeExercise/TestProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonTypeExercise
+{
+    class TestProgram
+    {
+        static void Main()
+        {
+            Country bg = new Country("Bulgaria", 7100000, 111000, "Sofia", "Plovdiv", "Varna");
+        }
+    }
+}
