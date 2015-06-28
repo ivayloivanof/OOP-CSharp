f308865c5776f279282967c7e67357088c80527d
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2015-06-28 21:50:37 +0300

    Add CommonTypeSystem Homework and presentation

---
+++ 10.CommonType/Homework/02.Customer/CustomerMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CustomerMain
+{
+    private static void Main()
+    {
+        var payment = new Payment("New", 10);
+
+        var zarko = new Customer(
+            "Dimitri",
+            "Vlaknov",
+            4564684654,
+            085965400,
+            "dimitri@babene.ru",
+            CustomerType.OneTime,
+            payment);
+
+        var pesho = new Customer(
+            "Pesho",
+            "Goshov",
+            6599464684,
+            0799564489,
+            "pesho@gmail.com",
+            CustomerType.Diamond);
+
+        var damqn = zarko.Clone() as Customer;
+        damqn.FirstName = "Damqn";
+        damqn.LastName = "Ivanov";
+
+        var asen = pesho.Clone() as Customer;
+        asen.Id = 8234353535;
+        asen.LastName = "Haralampiev";
+
+        Console.WriteLine(damqn);
+        Console.WriteLine(zarko);
+        
+        var customers = new List<Customer> { zarko, damqn, asen, pesho };
+        customers.Sort();
+
+        Console.WriteLine(string.Join(", ", customers));
+    }
+}
---
+++ 10.CommonType/Homework/02.Customer/CustomerType.cs
@@ -0,0 +1,7 @@
+﻿public enum CustomerType
+{
+    OneTime,
+    Regular,
+    Golden,
+    Diamond
+}
